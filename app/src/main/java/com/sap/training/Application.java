8c5892c473adb49ae4c4de74304277e74f275610
I740363 <pratham.sharma@sap.com>
2026-08-28 16:00:37 +0530

    fgfgfgf

--- app/src/main/java/com/sap/training/Application.java
+++ app/src/main/java/com/sap/training/Application.java
@@ -12,4 +12,13 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 }
-//asdfasdfhhhhhkyufgjfghjfjyfghh
+////asdfasdfhhhhhkyufgjfghjfjyfghh
+//F_API:  https://api.cf.us10-001.hana.ondemand.com
+//CF_ORG:  43b9d6a6trial
+//CF_SPACE:  dev
+//CF_REFRESH_TOKEN: b1aa90fd6d8040c4bdabf334d4f6c258-r
+//CF_UAA_URL:  https://uaa.cf.us10-001.hana.ondemand.com
+//CF_AUTH_URL:  https://login.cf.us10-001.hana.ondemand.com
+//CF_DOPPLER_URL:  wss://doppler.cf.us10-001.hana.ondemand.com:443
+//CF_LOG_CACHE_URL: https://log-cache.cf.us10-001.hana.ondemand.com
+//CF_UAA_ENDPOINT:  https://uaa.cf.us10-001.hana.ondemand.com
