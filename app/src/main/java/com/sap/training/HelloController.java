35e782940aec256dba987f9687eee745a1fcda1c
I740363 <pratham.sharma@sap.com>
2026-08-28 14:59:04 +0530

    dobnenenenenen

--- app/src/main/java/com/sap/training/HelloController.java
+++ app/src/main/java/com/sap/training/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from SfgjhgjAP DevOps Training!";
+        return "Hello from SfgjhgjasdfasdfAP DevOps Training!";
     }
 }
